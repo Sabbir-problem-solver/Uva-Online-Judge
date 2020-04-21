20db215d4a35813285f268725d766c8f48e4f40d
Sabbir-problem-solver <63328834+Sabbir-problem-solver@users.noreply.github.com>
2020-04-22 01:45:32 +0600

    Create A Change in Thermal Unit.cpp

---
+++ A Change in Thermal Unit.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int main()
+{
+
+    int i=0,n,tc;
+    scanf("%d",&tc);
+
+    while(tc--)
+    {
+            float c,f,C;
+            scanf("%f %f",&c,&f);
+            C=((5.0/9.0)*f)+c;
+            printf("Case %d: %.2f\n",++i,C);
+
+
+    }
+
+}
